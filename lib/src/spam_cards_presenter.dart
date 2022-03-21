4a8d99ce299daeaf2079a932c1404e4912ab7ff0
Ricardo Gonçalves <ricardo@mytiki.com>
2022-03-21 19:06:05 -0300

    add show security score

--- lib/src/spam_cards_presenter.dart
+++ lib/src/spam_cards_presenter.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:spam_cards/src/model/security_score_modal_model.dart';
 import 'package:spam_cards/src/spam_cards_service.dart';
 
+import 'ui/security_score_modal/security_score_modal_layout.dart';
+
 class SpamCardsPresenter {
   SpamCardsService service;
   SpamCardsPresenter(this.service);
 
-  showModal(
-      BuildContext context, SecurityScoreModalModel securityScoreModalModel) {}
+  Future<void> showModal(BuildContext context, SecurityScoreModalModel model) {
+    return showModalBottomSheet<void>(
+        context: context,
+        isScrollControlled: true,
+        isDismissible: true,
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+                top: Radius.circular(service.style.size(26)))),
+        builder: (BuildContext context) =>
+            SecurityScoreModalLayout(model, service.style));
+  }
 }
